8c5ef2903fad6e4fcf8ab3db67c40ef3084c89fc
Sarthak <sarthak242@gmail.com>
2021-06-08 16:07:30 +0530

    Feat: added link to profile page

--- frontend/src/shared-components/user-info/UserInfo.tsx
+++ frontend/src/shared-components/user-info/UserInfo.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import styles from './UserInfo.module.css';
 
-export function UserInfo({ name, headline="", postedOn }: { name: string, headline?: string, postedOn?: string }) {
+export function UserInfo({ userId, name, headline="", postedOn }: { userId: string, name: string, headline?: string, postedOn?: string }) {
 
     return (
         <div className={ styles.userInfo }>
-           <strong className={ styles.author }>{ name }</strong>
+           <Link to={`/profile/${userId}`} className={ styles.author }>{ name }</Link>
            <p className={ styles.metaInfo }>{ headline }</p>
            <p className={ styles.metaInfo }>{ postedOn }</p>
         </div>
