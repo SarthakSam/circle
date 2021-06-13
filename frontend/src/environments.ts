dc4869b56352b17f27e18d544420663ee9b23903
Sarthak <sarthak242@gmail.com>
2021-06-13 17:27:53 +0530

    Fix: changed app title

--- frontend/src/environments.ts
+++ frontend/src/environments.ts
@@ -2,7 +2,7 @@ const localUrl: string = "http://localhost:3000";
 const prodUrl: string = "https://buddees.netlify.app";
 
 const localApiUrl: string = "";
-const prodApiUrl: string = "https://buddees.herokuapp.com/";
+const prodApiUrl: string = "https://buddees.herokuapp.com";
 
 export const redirectUrl = prodUrl;
 export const baseURL = prodApiUrl;
