0712e435a5d9b4a986a2b725b988075d62ef4265
Sarthak <sarthak242@gmail.com>
2021-06-13 16:58:06 +0530

    Fix: fixing signup in case of google oauth user

--- backend/apis/createUser.api.js
+++ backend/apis/createUser.api.js
@@ -6,11 +6,20 @@ router.post('/', async (req, res, next) => {
     const user = req.body;
     console.log("logging this message for debugging", {user});
     try {
-        const newUser = await User.create(user);
-        if(newUser) {
-            return res.status(201).json({ message: `Hello`, user: newUser });
+        const alreadyUser = await User.findOne({ email: user.email });
+        if(alreadyUser) {
+            if(alreadyUser.googleId !== user.googleId) {
+                alreadyUser.googleId = user.googleId;
+                await alreadyUser.save();
+                return res.status(201).json({ message: 'Hello', user: alreadyUser });
+            }
+        } else {
+            const newUser = await User.create(user);
+            if(newUser) {
+                return res.status(201).json({ message: `Hello`, user: newUser });
+            }
+            return res.status(500).json({ errorMessage: "Something went wrong." });
         }
-        res.status(500).json({ errorMessage: "Something went wrong." });
     } catch(err) {
         console.log(err);
         next(err);
