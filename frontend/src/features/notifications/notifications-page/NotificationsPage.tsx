39026d5efcec9656b13b876a456492a7892dd357
Sarthak <sarthak242@gmail.com>
2021-06-13 19:22:05 +0530

    Fix: fixed notifications page wrong url style: notFound page

--- frontend/src/features/notifications/notifications-page/NotificationsPage.tsx
+++ frontend/src/features/notifications/notifications-page/NotificationsPage.tsx
@@ -9,40 +9,47 @@ export function NotificationsPage() {
     const notifications = useAppSelector(selectNotifications);
 
     return (
-        <ul className={ `col-5 col-lg-7 col-md-8 col-sm-10 ${ styles.notifications }` }>
+        <>
+        {
+            notifications.length > 0? 
+            <ul className={ `col-5 col-lg-7 col-md-8 col-sm-10 ${ styles.notifications }` }>
             {
                 notifications.map( notification  => <li className={ styles.notification }>
                     <Avatar size={40} src={ notification.notificationBy.profilePic?.url } />
                     {
                         notification.type === 'COMMENT' &&
                         <p className="text--lead">
-                             <Link to={ `post/${ notification.extraInfo.post }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
-                            commented on your <Link to={ `post/${ notification.extraInfo.post }` } >post</Link>.
+                             <Link to={ `/profile/${ notification.notificationBy._id }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
+                            commented on your <Link to={ `/post/${ notification.extraInfo.post }` } >post</Link>.
                         </p>
                     }
                     {
                         notification.type === 'LIKE' &&
                         <p className="text--lead">
-                            <Link to={ `post/${ notification.extraInfo.post }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
-                            liked your <Link to={ `post/${ notification.extraInfo.post }` } >post</Link>.
+                            <Link to={ `/profile/${ notification.notificationBy._id }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
+                            liked your <Link to={ `/post/${ notification.extraInfo.post }` } >post</Link>.
                         </p>
                     }
                     {
                         notification.type === 'FRIEND_REQUESTED' &&
                         <p className="text--lead">
-                            <Link to={ `post/${ notification.extraInfo.post }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
+                            <Link to={ `/profile/${ notification.notificationBy._id }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
                             sent you friend request.
                         </p>
                     }
                     {
                         notification.type === 'FRIEND_REQUEST_ACCEPTED' &&
                         <p className="text--lead">
-                            <Link to={ `post/${ notification.extraInfo.post }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
+                            <Link to={ `/profile/${ notification.notificationBy._id }` } > { `${ notification.notificationBy.firstname } ${ notification.notificationBy.lastname }` } </Link>
                             accepted your friend request.
                         </p>
                     }
                 </li> )
             }
         </ul>
+        :
+            <p>No Notifications for now.</p>
+        }
+        </>
     )
 }
